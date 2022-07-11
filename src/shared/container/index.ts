e8007a56a2e3e8b02384897c41b38324aed791e9
Renan Xavier <renansxdev@gmail.com>
2022-07-11 19:19:52 -0300

    criado modules de users (account)

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { container } from "tsyringe";
+import { UsersRepository }  from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository }  from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoryRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
 // ICategoriesRepository
 container.registerSingleton<ICategoryRepository>(
@@ -14,4 +16,10 @@ container.registerSingleton<ICategoryRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+);
+
+// IUsersRepository
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
