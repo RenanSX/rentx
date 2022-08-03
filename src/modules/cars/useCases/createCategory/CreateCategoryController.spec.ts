71e59c98698de03d99fb7c3dfd0a7519f5681f7b
Renan Xavier <renansxdev@gmail.com>
2022-08-03 18:23:28 -0300

    criado teste de integraçao com supertest para create category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,65 @@
+import request from "supertest";
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+import { hash } from "bcrypt";
+
+let connection: Connection;
+describe("Create Category Controller", () => {
+
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+        VALUES('${id}', 'Admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXXXX')
+      `);
+  });
+
+  afterAll( async() => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to create a new category", async () => {
+    const reponseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin"
+    });
+    const response = await request(app)
+    .post("/categories")
+    .set({
+      Authorization: `Bearer ${reponseToken.body.token}`
+    })
+    .send({
+      name: "category super test",
+      description: "Categoria de carros SUV - SUPER TEST"
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a new category with name exists", async () => {
+    const reponseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin"
+    });
+
+    const response = await request(app)
+    .post("/categories")
+    .set({
+      Authorization: `Bearer ${reponseToken.body.token}`
+    })
+    .send({
+      name: "category super test",
+      description: "Categoria de carros SUV - SUPER TEST"
+    });
+
+    expect(response.status).toBe(400);
+  });
+});
